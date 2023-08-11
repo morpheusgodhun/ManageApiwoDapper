66caf8b86f7fa69bba06b5cfb0053ae01c1ea7a2
Orhun Yusuf Babaoglu <74192702+morpheusgodhun@users.noreply.github.com>
2023-08-11 16:56:07 +0300

    Navbarpartial layout progress.

---
+++ RealEstate_Dapper_UI/Views/Shared/Components/_NavbarViewComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Views.Shared.Components {
+    public class _NavbarViewComponentPartial:ViewComponent {
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
